40b19e2ce7edf5d3798dbf7e02b3cbd591d241eb
Michael Ortali <michael.ortali@gmail.com>
2024-05-19 12:11:53 -0400

    Add a context used throughout the application to store the features, scenarios, and tests.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import {AppHeader} from "./components/header/AppHeader.tsx";
 import {AppSidebar} from "./components/sidebar/AppSidebar.tsx";
 import {AppFeatures} from "./components/features/AppFeatures.tsx";
 import {AppScenario} from "./components/scenario/AppScenario.tsx";
+import {ContextProvider} from "./state/context.tsx";
 
 function App() {
   return (
     <div className={css.App}>
-      <AppHeader />
-      <div className={css.AppBody}>
-        <AppSidebar />
-        <AppFeatures />
-        <AppScenario />
-      </div>
+      <ContextProvider>
+        <AppHeader/>
+        <div className={css.AppBody}>
+          <AppSidebar/>
+          <AppFeatures/>
+          <AppScenario/>
+        </div>
+      </ContextProvider>
     </div>
   )
 }
---
+++ src/state/context.tsx
@@ -0,0 +1,50 @@
+import {createContext, Dispatch, SetStateAction, useContext, useState} from "react";
+
+/**
+ * Interface for the context. Adding new elements to the context should
+ * be listed in this interface, and it will be broadly available throughout
+ * the application.
+ */
+export interface ContextInterface {
+}
+
+/**
+ * Defaults getters and setters for the context. This allows typescript to
+ * be aware of the different types without strictly requiring them.
+ */
+export const ContextState = createContext({
+  state: {} as Partial<ContextInterface>,
+  setState: {} as Dispatch<SetStateAction<Partial<ContextInterface>>>,
+});
+
+/**
+ * React element that encapsulates the current application to make
+ * the context broadly available.
+ */
+export const ContextProvider = (
+  {
+    children,
+    value = {} as ContextInterface,
+  }: {
+    children: React.ReactNode;
+    value?: Partial<ContextInterface>;
+  }
+) => {
+  const [state, setState] = useState(value);
+  return (
+    <ContextState.Provider value={{state, setState}}>
+      {children}
+    </ContextState.Provider>
+  );
+};
+
+/**
+ * Returns the current application context.
+ */
+export const useContextState = () => {
+  const context = useContext(ContextState);
+  if (!context) {
+    throw new Error("useContextState must be used within a ContextState");
+  }
+  return context;
+};
