d9b679cd12ead375905193789a7377f4b1a9d147
Michael Ortali <michael.ortali@gmail.com>
2024-05-19 14:01:58 -0400

    Make the side card sticky.

--- src/components/scenario/AppScenario.tsx
+++ src/components/scenario/AppScenario.tsx
@@ -3,15 +3,17 @@ import {Scenario} from "../../state/store.ts";
 import {ScenarioIcon} from "../../assets/icons/ScenarioIcon.tsx";
 import {AppScenarioStep} from "./step/AppScenarioStep.tsx";
 
-export function AppScenario(props: {scenario: Scenario}) {
+export function AppScenario(props: { scenario: Scenario }) {
   return <div className={css.AppScenario} key={props.scenario.name}>
-    <h2>
-      <ScenarioIcon />
-      {props.scenario.name}
-    </h2>
+    <div className={css.AppScenarioInner}>
+      <h2>
+        <ScenarioIcon/>
+        {props.scenario.name}
+      </h2>
 
-    <div className={css.AppScenarioSteps}>
-      {props.scenario.steps.map((step) => <AppScenarioStep step={step} />)}
+      <div className={css.AppScenarioSteps}>
+        {props.scenario.steps.map((step) => <AppScenarioStep step={step}/>)}
+      </div>
     </div>
   </div>
 }
