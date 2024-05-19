ec272d3945906e9488aa6a59feaa8c3b403a83db
Michael Ortali <michael.ortali@gmail.com>
2024-05-19 13:35:28 -0400

    Add layout to the side card.

--- src/components/scenario/AppScenario.tsx
+++ src/components/scenario/AppScenario.tsx
@@ -3,6 +3,6 @@ import {Scenario} from "../../state/store.ts";
 
 export function AppScenario(props: {scenario: Scenario}) {
   return <div className={css.AppScenario} key={props.scenario.name}>
-    {props.scenario.name}
+    <h2>{props.scenario.name}</h2>
   </div>
 }
