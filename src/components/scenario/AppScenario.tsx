7086f9ef8fda11c9fb824f2d5d4f2ffaf5efa470
Michael Ortali <michael.ortali@gmail.com>
2024-05-19 13:38:31 -0400

    Add icon to the side scenario card.

--- src/components/scenario/AppScenario.tsx
+++ src/components/scenario/AppScenario.tsx
@@ -1,8 +1,12 @@
 import css from './AppScenario.module.css'
 import {Scenario} from "../../state/store.ts";
+import {ScenarioIcon} from "../../assets/icons/ScenarioIcon.tsx";
 
 export function AppScenario(props: {scenario: Scenario}) {
   return <div className={css.AppScenario} key={props.scenario.name}>
-    <h2>{props.scenario.name}</h2>
+    <h2>
+      <ScenarioIcon />
+      {props.scenario.name}
+    </h2>
   </div>
 }
