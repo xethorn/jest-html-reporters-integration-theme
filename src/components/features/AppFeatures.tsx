19e166fe127358d520f25525469508d74cf388b7
Michael Ortali <michael.ortali@gmail.com>
2024-05-19 12:49:13 -0400

    Add icon next to the feature name.

--- src/components/features/AppFeatures.tsx
+++ src/components/features/AppFeatures.tsx
@@ -1,5 +1,6 @@
 import css from './AppFeatures.module.css'
 import {useContextState} from "../../state/context.tsx";
+import {FeatureIcon} from "../../assets/icons/FeatureIcon.tsx";
 
 export function AppFeatures() {
   const {state} = useContextState()
@@ -7,7 +8,10 @@ export function AppFeatures() {
   return <div className={css.AppFeatures}>
     {state.features?.map((feature) =>
       <div id={`feature-${feature.name}`} className={css.AppFeature}>
-        <h2 className={css.AppFeatureName}>{feature.name}</h2>
+        <h2 className={css.AppFeatureName}>
+          <FeatureIcon />
+          {feature.name}
+        </h2>
       </div>
     )}
   </div>
