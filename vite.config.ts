61f06b6d7dfce9b9a6bff9a34d409915de0cb33d
Michael Ortali <michael.ortali@gmail.com>
2024-05-19 22:25:40 -0400

    Add build dirs.

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,13 @@ export default defineConfig({
     noAttr()
   ],
   build: {
-    assetsDir: '.'
+    assetsDir: '.',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
   }
 })
